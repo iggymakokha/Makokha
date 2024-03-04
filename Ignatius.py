6b0943b89ae186d2ad9144520c248b5860fe5cf5
iggymakokha <145118243+iggymakokha@users.noreply.github.com>
2024-03-04 22:31:18 +0300

    Add files via upload

---
+++ Ignatius.py
@@ -0,0 +1,5 @@
+name= input("enter your name:")
+age= input("enter your age:")
+location= input("enter your location")
+message= f"Hello. My names is {name}. I am {age} years old and I come from {location}. Au revoir"
+print(message)
